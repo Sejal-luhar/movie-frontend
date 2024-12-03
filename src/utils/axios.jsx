b4a453b3c9dd07eeb3fd1b7e4dd1b90d7c0bf90e
Sejal-luhar <sejalluhar2001@gmail.com>
2024-12-03 13:56:59 +0530

    new one

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_REACT_APP_BASEURL}`, // Backend URL
+  baseURL: `http://localhost:5000`, // Backend URL
   withCredentials: true, // Include cookies for session management
 });
 
