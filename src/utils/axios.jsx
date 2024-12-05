22ba2d6ae594e0327e27748f5f673c5b4be553d5
Sejal-luhar <sejalluhar2001@gmail.com>
2024-12-05 12:08:28 +0530

    last

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL, // Replace with your backend API URL
+  baseURL: `https://movie-backend-eaar.onrender.com`, // Replace with your backend API URL
   withCredentials: true, // Include cookies for session management
 });
 
