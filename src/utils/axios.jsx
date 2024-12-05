63fad62c517f614ed8a5b9d4ebb99835ef8fbe48
Sejal-luhar <sejalluhar2001@gmail.com>
2024-12-05 12:08:09 +0530

    baseurl

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000', // Replace with your backend API URL
+  baseURL: process.env.REACT_APP_BACKEND_URL, // Replace with your backend API URL
   withCredentials: true, // Include cookies for session management
 });
 
